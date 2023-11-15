cf076f27fb4f09f07634c79aae6d668fa9cfdf43
Universe-E <zehuakong.uranus@gmail.com>
2023-11-16 00:32:48 +1100

    feat: add axios test: axios

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,22 @@
   <Pagination></Pagination>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import request from '@/utils/request.ts'
+
+onMounted(() => {
+  request({
+    url: '/user/login',
+    method: 'post',
+    data: {
+      username: 'admin',
+      password: '111111',
+    },
+  }).then((res) => {
+    console.log(res)
+  })
+})
+</script>
 
 <style scoped lang="scss"></style>
---
+++ src/utils/request.ts
@@ -0,0 +1,44 @@
+import axios from 'axios'
+import { ElMessage } from 'element-plus'
+//create axios instances
+const request = axios.create({
+  baseURL: import.meta.env.VITE_APP_BASE_API,
+  timeout: 5000,
+})
+//request拦截器
+request.interceptors.request.use((config) => {
+  return config
+})
+//response拦截器
+request.interceptors.response.use(
+  (response) => {
+    return response.data
+  },
+  (error) => {
+    //handle network error
+    let msg = ''
+    const status = error.response.status
+    switch (status) {
+      case 401:
+        msg = 'Invalid Token'
+        break
+      case 403:
+        msg = 'Forbidden'
+        break
+      case 404:
+        msg = 'Not Found'
+        break
+      case 500:
+        msg = 'Server Error'
+        break
+      default:
+        msg = 'Network Unavailable'
+    }
+    ElMessage({
+      type: 'error',
+      message: msg,
+    })
+    return Promise.reject(error)
+  },
+)
+export default request
